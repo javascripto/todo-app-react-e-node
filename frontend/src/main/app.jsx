3b1a58f7a47c99aed2b79e3751dcdf91a8d1f993
Yuri Alves <yurialves2@gmail.com>
2019-10-17 20:07:13 -0300

    init frontend - webpack config

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+
+
+export default props => {
+  return (
+    <div className="container">
+      <h1>Olá, Mundo!</h1>
+    </div>
+  );
+}
